c2fb8f5db8a83276d99860934fa85da80facf98f
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-05 02:02:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,10 +35,7 @@ def using_uniq(array)
   array.uniq()
 end
 
-def using_flatten(array)
-  def using_delete(array1, str)
-  array1.delete(str)
+ def using_flatten(array)
+  array.flatten
 end
 
- def using_delete_at(array1, int)
-  array1.delete_at(int)
